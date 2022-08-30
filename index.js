108483adc54a4e8ffa53d7c55de69187429f65a7
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-08-30 11:34:20 +0300

    log

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
     const contract = new ethers.Contract('0xE1A97980e5691AE8Dc99B458e0Dd705e50E85EE5', contractAbi, rinkebyAlchemyProvider)
     console.log('Server running');
     contract.on('Transfer', async (from, to, token) => {
+        console.log(`Event fired with: from:${from}, to:${to}, token:${ethers.BigNumber.from(token).toNumber()}`)
         const tokenUri = await contract.tokenURI(ethers.BigNumber.from(token).toNumber())
         await fetch(`https://u6i3kt6ir7.execute-api.eu-west-3.amazonaws.com/dev/handleTransferEvent`, {
             method: "POST",
