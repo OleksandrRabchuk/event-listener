557e732e7042a18129f8350bf9ace738c8dafff1
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-08-30 11:25:15 +0300

    done

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const ethers = require('ethers')
 const contractAbi = require("./abi.json");
+const fetch = require('node-fetch');
 
-module.exports.run = async () => {
+(async () => {
     const rinkebyAlchemyProvider = new ethers.providers.JsonRpcProvider('https://eth-rinkeby.alchemyapi.io/v2/iRkguVLmnNufz3fZbDThtEgK-lcxI5uF')
     const contract = new ethers.Contract('0xE1A97980e5691AE8Dc99B458e0Dd705e50E85EE5', contractAbi, rinkebyAlchemyProvider)
+    console.log('Server running');
     contract.on('Transfer', async (from, to, token) => {
         const tokenUri = await contract.tokenURI(ethers.BigNumber.from(token).toNumber())
         await fetch(`https://u6i3kt6ir7.execute-api.eu-west-3.amazonaws.com/dev/handleTransferEvent`, {
             method: "POST",
-            "Content-Type":"application/json",
-            "body": JSON.stringify({
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
                 from,
                 to,
                 tokenId: ethers.BigNumber.from(token).toNumber(),
@@ -17,4 +19,5 @@ module.exports.run = async () => {
             })
         })
     })
-}
+
+})()
