a002a84dd39ba8752833ab099c39e302905d2c57
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-09-13 13:33:30 +0300

    contract change

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const contractAbi = require("./abi.json");
 const fetch = require('node-fetch');
 
 (async () => {
-    const rinkebyAlchemyProvider = new ethers.providers.JsonRpcProvider('https://eth-rinkeby.alchemyapi.io/v2/iRkguVLmnNufz3fZbDThtEgK-lcxI5uF')
-    const contract = new ethers.Contract('0xE1A97980e5691AE8Dc99B458e0Dd705e50E85EE5', contractAbi, rinkebyAlchemyProvider)
+    const provider = ethers.getDefaultProvider('goerli')
+    const contract = new ethers.Contract('0xad464BCBdF3C9d835364be22B45A696B2440c7c0', contractAbi, provider)
     console.log('Server running');
     contract.on('Transfer', async (from, to, token) => {
-        console.log(`Event fired with: from:${from}, to:${to}, token:${ethers.BigNumber.from(token).toNumber()}`)
         const tokenUri = await contract.tokenURI(ethers.BigNumber.from(token).toNumber())
         await fetch(`https://u6i3kt6ir7.execute-api.eu-west-3.amazonaws.com/dev/handleTransferEvent`, {
             method: "POST",
