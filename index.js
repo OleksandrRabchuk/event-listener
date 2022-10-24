53831a4a494c1c4d12c16ac0ddd3dae71b059ed0
OleksandrRabchuk <oleksandr.rabchuk@progforce.com>
2022-10-24 18:29:39 +0300

    fix

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
     const contract = new ethers.Contract('0x9F83b08D90eeDa539f7E2797fEd3f6996917BbA8', contractAbi, provider)
     contract.on('Transfer', async (from, to, token) => {
         const tokenUri = await contract.tokenURI(ethers.BigNumber.from(token).toNumber())
+        console.log({from,to,tokenUri})
         await fetch(`https://8cyq9zjqx7.execute-api.us-east-2.amazonaws.com/handleTransferEvent`, {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
